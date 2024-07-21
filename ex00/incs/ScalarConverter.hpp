63b8371362d598acf28f7c9348fc8479e5399082
Mimenza <mimenzae@gmail.com>
2024-07-21 21:29:55 +0200

    ex00

--- ex00/incs/ScalarConverter.hpp
+++ ex00/incs/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: emimenza <emimenza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 11:27:27 by emimenza          #+#    #+#             */
-/*   Updated: 2024/07/21 19:34:27 by emimenza         ###   ########.fr       */
+/*   Updated: 2024/07/21 21:28:24 by emimenza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,14 @@ class ScalarConverter
 {
 
     public:
+        static void convert(std::string const &literal);
+    private:
         ScalarConverter();
         ScalarConverter(ScalarConverter &source);
         virtual ~ScalarConverter(void);
 
         ScalarConverter &operator=(ScalarConverter &source);
 
-        static void convert(std::string const &literal);
 };
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: emimenza <emimenza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 11:32:44 by emimenza          #+#    #+#             */
-/*   Updated: 2024/07/21 19:30:09 by emimenza         ###   ########.fr       */
+/*   Updated: 2024/07/21 21:28:37 by emimenza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 int main (int argc, char **argv)
 {
-    ScalarConverter sc;
-
     if (argc != 2)
         return (std::cerr << "Error, usage: ./executable input" << std::endl, 0);
     std::string literal = argv[1];
 
-    sc.convert(literal);
+    ScalarConverter::convert(literal);
 
 }
